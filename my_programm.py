d88ce45580a1815746e92bc602d030762aa2d963
alin <alin.burchiu@gmail.com>
2017-01-11 16:09:34 +0100

    print name only is alfa

--- my_programm.py
+++ my_programm.py
@@ -1,6 +1,12 @@
 name = raw_input("Please enter yor name: ")
 
-if name:
+def is_valid(name):
+	if name.isalpha():
+		return True
+	return False
+
+
+if is_valid(name):
 	print 'hello', name
 else:
 	print 'hello world'
