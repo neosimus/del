652827e7b452db3f91714f1a8b080fff04e73d5a
alin <alin.burchiu@gmail.com>
2017-01-11 16:29:12 +0100

    fix bug in double-barrelled names

--- my_programm.py
+++ my_programm.py
@@ -1,8 +1,9 @@
 name = raw_input("Please enter yor name: ")
 
 def parse_name(name):
-	if all(n.strip().isalpha() for n in name.split('-')):
-		return name.lower().capitalize()
+	names = [n.strip() for n in name.split("-")]
+	if all(n.isalpha() for n in names):
+		return '-'.join(n.lower().capitalize() for n in names)
 	else:
 		return 'world'
 
