4054a148c4d210164c16d981356059fb03965c87
alin <alin.burchiu@gmail.com>
2017-01-11 16:23:56 +0100

    double-barelled names

--- my_programm.py
+++ my_programm.py
@@ -1,7 +1,7 @@
 name = raw_input("Please enter yor name: ")
 
 def parse_name(name):
-	if name.isalpha():
+	if all(n.strip().isalpha() for n in name.split('-')):
 		return name.lower().capitalize()
 	else:
 		return 'world'
