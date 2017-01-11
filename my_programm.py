fba9c02da90f93a121bdb291d1230144b921d8f2
alin <alin.burchiu@gmail.com>
2017-01-11 16:47:35 +0100

    spaces in names

--- my_programm.py
+++ my_programm.py
@@ -1,11 +1,12 @@
 name = raw_input("Please enter yor name: ")
 
 def parse_name(name):
-	names = [n.strip() for n in name.split("-")]
-	if all(n.isalpha() for n in names):
-		return '-'.join(n.lower().capitalize() for n in names)
-	else:
-		return 'world'
-
+	names = name.split("-")
+	for i in range(len(names)):
+		names_with_particles = names[i].split()
+		if not all(n.isalpha() for n in names_with_particles):
+			return 'world'
+		names[i] = " ".join(n.lower().capitalize() for n in names_with_particles)
+	return '-'.join(names)
 
 print 'hello', parse_name(name)
